b5be1e19f1397dd2f41eb8a99f83141ad72b64be
bill.lin <zzx123bill@gmail.com>
2024-06-19 22:37:19 +0800

    setup project

---
+++ src/test/java/com/bill/multipleDatasource/MultipleDatasourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bill.multipleDatasource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultipleDatasourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
